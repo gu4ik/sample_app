655e73796306f3b435e85a4605a32628c7f91872
Dmitriy g4k <gu4ikd7@bk.ru>
2018-09-28 16:15:39 +0500

    add static_page Contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,12 +10,13 @@ require 'rails_helper'
 #end
 
 describe "Static pages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   
   describe "Home page" do
    
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
     it "should have the content 'Sample App'" do
@@ -28,7 +29,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
     it "should have the content 'Help'" do
@@ -41,7 +42,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
 
     it "should have the content 'About Us'" do
@@ -50,6 +51,18 @@ describe "Static pages" do
     end
   end
 
+  describe "Contact page" do
 
+    it "should have the right title" do
+      visit 'static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
+    it "should have the content 'Contact'" do
+      visit 'static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+  end
 
 end
